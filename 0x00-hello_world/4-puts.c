793c8e7c92a3e51d0625e872c79e1f631858db83
Assane Faye <fayeassanemail.com>
2022-09-08 13:05:53 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-
 /**
+ * main - Print string in console
  *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
-
+ * Return: 0 to terminate program
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
