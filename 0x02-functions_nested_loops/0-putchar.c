fa8359d301961e8c89c6d8e88d9696ff62ceca1b
Assane Faye <fayeassanemail.com>
2022-09-13 09:13:41 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * main - Print "main" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char main[] = "main";
-	int i = 0;
-
-	while (main[i] != '\0')
-	{
-		_putchar(main[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
