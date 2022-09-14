cf9aeb5b32af987bd3b2c7b05b5570237a752bee
Assane Faye <fayeassanemail.com>
2022-09-14 12:56:26 -0700

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: return the result of a + b
+ *
+ * This function will add two given parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
