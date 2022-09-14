d171ef414c5c0a8c7150f9fa1944c1fbd7273b7d
Assane Faye <fayeassanemail.com>
2022-09-14 12:10:56 -0700

    test

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @c: the parameter to retur
+ * its absolute value
+ *
+ * Return: the absolute value
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else
+		return (c * -1);
+}
