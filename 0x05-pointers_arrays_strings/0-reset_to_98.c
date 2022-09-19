30117ee3b121064f4b284f0ba3adb02b5544af90
Assane Faye <fayeassanemail.com>
2022-09-19 14:22:23 -0700

    test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updating a value using pointers
+ * @n: * before n, n is a pointer as parameter
+ *
+ * Return: no return value
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
